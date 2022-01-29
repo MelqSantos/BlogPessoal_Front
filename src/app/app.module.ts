1f1cd98e6261f960fa9b29a7de385e4fb7d6a394
Melqui Santos <melqsantos96@gmail.com>
2022-01-29 00:19:39 -0300

    CRUD de postagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { FeedComponent } from './feed/feed.component';
 import { TemasComponent } from './temas/temas.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 
 @NgModule({
@@ -36,6 +38,8 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
     TemasComponent,
     TemaEditComponent,
     TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
   ],
   imports: [
     BrowserModule,
